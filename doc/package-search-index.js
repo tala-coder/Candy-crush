925e933c35302b1ccd5c7adb3ad429d4164627bc
Muhammed Talić <muhammedtalic.it@gmail.com>
2021-07-06 00:50:17 +0200

    Delete doc directory

--- doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"GUI"},{"l":"Konzola"},{"l":"Logika"}]
--- doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"Logika","l":"CandyCrush"},{"p":"GUI","l":"IgrajCandy"},{"p":"Konzola","l":"IgrajCandy"},{"p":"GUI","l":"MojPanel"},{"p":"GUI","l":"Nextlvl"},{"p":"GUI","l":"Pocetna"},{"p":"GUI","l":"Podloga"},{"p":"GUI","l":"Rezultat"}]
